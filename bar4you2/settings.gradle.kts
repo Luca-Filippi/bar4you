a1b65b0f90e534c596b869d6c5f348f21a6b8d3a
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-08-21 17:46:15 +0200

    Add files via upload

---
+++ bar4you2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "bar4you"
+include(":app")
